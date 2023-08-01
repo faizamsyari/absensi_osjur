b9068a17ee2027faaa9ccd948b76a690cff39cdf
bakpaokeju <halosemua88292@gmail.com>
2023-08-01 08:42:59 +0700

    tambahan logic

--- lib/app/modules/edit_data/controllers/edit_data_controller.dart
+++ lib/app/modules/edit_data/controllers/edit_data_controller.dart
@@ -11,12 +11,81 @@ class EditDataController extends GetxController {
     String? tgl,
     var textmasuk,
   ) async {
+    final cekawal = await firestore
+        .collection("Pengguna")
+        .doc(uid["id"])
+        .collection("Presensi")
+        .doc(tgl)
+        .get();
+
     await firestore
         .collection("Pengguna")
         .doc(uid["id"])
         .collection("Presensi")
         .doc(tgl)
         .update({"Status": textmasuk});
+
+    if (textmasuk == "Sakit" && cekawal.data()?["Status"] == "Izin") {
+      final ceksakit = await firestore.collection("rekapan").doc(tgl).get();
+      await firestore
+          .collection("rekapan")
+          .doc(tgl)
+          .update({"Sakit": ceksakit.data()?["Sakit"] + 1});
+      await firestore
+          .collection("rekapan")
+          .doc(tgl)
+          .update({"Izin": ceksakit.data()?["Izin"] - 1});
+    } else if (textmasuk == "Izin" && cekawal.data()?["Status"] == "Sakit") {
+      final cekizin = await firestore.collection("rekapan").doc(tgl).get();
+      await firestore
+          .collection("rekapan")
+          .doc(tgl)
+          .update({"Izin": cekizin.data()?["Izin"] + 1});
+      await firestore
+          .collection("rekapan")
+          .doc(tgl)
+          .update({"Sakit": cekizin.data()?["Sakit"] - 1});
+    } else if (textmasuk == "Izin" && cekawal.data()?["Status"] == "Hadir") {
+      final cekizin = await firestore.collection("rekapan").doc(tgl).get();
+      await firestore
+          .collection("rekapan")
+          .doc(tgl)
+          .update({"Izin": cekizin.data()?["Izin"] + 1});
+      await firestore
+          .collection("rekapan")
+          .doc(tgl)
+          .update({"Hadir": cekizin.data()?["Hadir"] - 1});
+    } else if (textmasuk == "Sakit" && cekawal.data()?["Status"] == "Hadir") {
+      final cekizin = await firestore.collection("rekapan").doc(tgl).get();
+      await firestore
+          .collection("rekapan")
+          .doc(tgl)
+          .update({"Sakit": cekizin.data()?["Sakit"] + 1});
+      await firestore
+          .collection("rekapan")
+          .doc(tgl)
+          .update({"Hadir": cekizin.data()?["Hadir"] - 1});
+    } else if (textmasuk == "Hadir" && cekawal.data()?["Status"] == "Sakit") {
+      final cekizin = await firestore.collection("rekapan").doc(tgl).get();
+      await firestore
+          .collection("rekapan")
+          .doc(tgl)
+          .update({"Hadir": cekizin.data()?["Hadir"] + 1});
+      await firestore
+          .collection("rekapan")
+          .doc(tgl)
+          .update({"Sakit": cekizin.data()?["Sakit"] - 1});
+    } else if (textmasuk == "Hadir" && cekawal.data()?["Status"] == "Izin") {
+      final cekizin = await firestore.collection("rekapan").doc(tgl).get();
+      await firestore
+          .collection("rekapan")
+          .doc(tgl)
+          .update({"Izin": cekizin.data()?["Izin"] + 1});
+      await firestore
+          .collection("rekapan")
+          .doc(tgl)
+          .update({"Hadir": cekizin.data()?["Hadir"] - 1});
+    }
     Get.back();
     Get.snackbar("Selamat !!!", "Data Berhasil Diupdate");
   }
--- lib/app/modules/read_presence/controllers/read_presence_controller.dart
+++ lib/app/modules/read_presence/controllers/read_presence_controller.dart
@@ -14,12 +14,50 @@ class ReadPresenceController extends GetxController {
         .snapshots();
   }
 
-  void deletePresence(Map<String, dynamic> uid, String? tgl) {
-    firestore
+  void deletePresence(Map<String, dynamic> uid, String? tgl) async {
+    final ceka = await firestore
+        .collection("Pengguna")
+        .doc(uid["id"])
+        .collection("Presensi")
+        .doc(tgl)
+        .get();
+
+    await firestore
         .collection("Pengguna")
         .doc(uid["id"])
         .collection("Presensi")
         .doc(tgl)
         .delete();
+
+    final cek = await firestore.collection("rekapan").doc(tgl).get();
+
+    if (ceka.data()?["Status"] == "Izin") {
+      await firestore
+          .collection("rekapan")
+          .doc(tgl)
+          .update({"Tidak Hadir": cek.data()?["Tidak Hadir"] + 1});
+      await firestore
+          .collection("rekapan")
+          .doc(tgl)
+          .update({"Izin": cek.data()?["Izin"] - 1});
+    } else if (ceka.data()?["Status"] == "Sakit") {
+      await firestore
+          .collection("rekapan")
+          .doc(tgl)
+          .update({"Tidak Hadir": cek.data()?["Tidak Hadir"] + 1});
+      await firestore
+          .collection("rekapan")
+          .doc(tgl)
+          .update({"Sakit": cek.data()?["Sakit"] - 1});
+    } else {
+      await firestore
+          .collection("rekapan")
+          .doc(tgl)
+          .update({"Tidak Hadir": cek.data()?["Tidak Hadir"] + 1});
+      await firestore
+          .collection("rekapan")
+          .doc(tgl)
+          .update({"Hadir": cek.data()?["Hadir"] - 1});
+    }
   }
 }
